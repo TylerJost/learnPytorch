e76385d44625035ce10af87f4cee37765e2a945f
Tyler Jost <tjost@login1.ls6.tacc.utexas.edu>
2022-07-31 13:23:03 -0500

    paired notebook

---
+++ testNotebook.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# This is a test
+
+# %%
+import numpy as np
+print('It\'s only a test!')
+
+# %%
