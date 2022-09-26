2fa1167ca82f5da4e2c7749561dbc185cc466289
Tyler Jost <tjost@login2.ls6.tacc.utexas.edu>
2022-09-26 10:11:19 -0500

    Added net to GPU

--- sentdex/convnet.py
+++ sentdex/convnet.py
@@ -6,8 +6,11 @@ import matplotlib.pyplot as plt
 # %matplotlib inline
 REBUILD_DATA=False
 
-
 # +
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("running on GPU")
+
 class DogsVSCats():
     IMG_SIZE = 50
     CATS = "PetImages/Cat"
@@ -123,32 +126,63 @@ train_y = y[:-val_size]
 test_X = X[val_size:]
 test_y = y[val_size:]
 
+# +
+BATCH_SIZE = 100
+EPOCHS = 1
+
+
+def train(net):
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    loss_function = nn.MSELoss()
+    for epoch in range(EPOCHS):
+        for i in tqdm(range(0, len(train_X), BATCH_SIZE)): # from 0, to the len of x, stepping BATCH_SIZE at a time. [:50] ..for now just to dev
+            #print(f"{i}:{i+BATCH_SIZE}")
+            # Generally not all the data can fit on the GPU
+            # So we send data to the 
+            
+            batch_X = train_X[i:i+BATCH_SIZE].view(-1, 1, 50, 50).to(device)
+            batch_y = train_y[i:i+BATCH_SIZE].to(device)
+            
+            net.zero_grad()
+
+            outputs = net(batch_X)
+            loss = loss_function(outputs, batch_y)
+            loss.backward()
+            optimizer.step()    # Does the update
+
+        print(f"Epoch: {epoch}. Loss: {loss}")
+
+
+def test(net):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for i in tqdm(range(len(test_X))):
+            real_class = torch.argmax(test_y[i]).to(device)
+            net_out = net(test_X[i].view(-1, 1, 50, 50).to(device))[0]  # returns a list, 
+            predicted_class = torch.argmax(net_out)
+
+            if predicted_class == real_class:
+                correct += 1
+            total += 1
+    print(f'Accuracy: {correct/total*100:0.2f}')
+
+
+
+# +
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("running on GPU")
+
+# Pytorch will not automatically convert everything for use on the GPU
+# Keep in mind tensors on the GPU will only be able to react with 
+net = Net().to(device)
 # -
 
-batch_size = 100
-epochs = 1
-for epoch in range(epochs):
-    for i in tqdm(range(0, len(train_X), batch_size)):
-        batch_X = train_X[i:i+batch_size].view(-1,1,50,50)
-        batch_y = train_y[i:i+batch_size]
-        
-        net.zero_grad()
-#         optim.zero_grad() # The optimizer controls all paramters for this model, so this is equivalent
-        outputs = net(batch_X)
-        loss = loss_function(outputs, batch_y)
-        loss.backward()
-        optimizer.step()
-    print(loss)
+EPOCHS = 20
+train(net)
+test(net)
 
-correct = 0
-total = 0
-with torch.no_grad():
-    for i in tqdm(range(len(test_X))):
-        real_class = torch.argmax(test_y[i])
-        net_out = net(test_X[i].view(-1,1,50,50))[0]
-        predicted_class = torch.argmax(net_out)
-        if predicted_class == real_class:
-            correct += 1
-        total += 1
 
-correct/total
+
+# ! squeue -u tjost
